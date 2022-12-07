39c1d81be1898ea214f5dc1cb87aa304de621048
Raphael Rego <raphaelcarlosr@mail.com>
2022-12-07 22:20:32 +0100

    remote setup

--- setup.sh
+++ setup.sh
@@ -6,13 +6,47 @@ DISK=${DISK:-"4G"}
 VM_NAME=${VM_NAME:-"microk8s"}
 export CPU MEMORY DISK VM_NAME
 
+USE_REMOTE_REPO=0
+if [ -z "${BASH_SOURCE:-}" ]; then
+    cmd="${CMD:-}"
+    BASE_DIR="${PWD}"
+    USE_REMOTE_REPO=1
+else
+    cmd="${1:-}"
+    BASE_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
+fi
+REPO_RAW_URL="${REPO_RAW_URL:-https://raw.githubusercontent.com/raphaelcarlosr/homelab-dev/main}"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=/dev/null
+
+function get_local_or_remote_file() {
+    if [ "${USE_REMOTE_REPO}" -eq 1 ]; then
+        echo "${REPO_RAW_URL}"
+    else
+        echo "${BASE_DIR}"
+    fi
+}
+
+function get_file() {
+    if [ "${USE_REMOTE_REPO}" -eq 1 ]; then
+        curl "$REPO_RAW_URL/${1}" # --output "${1}"
+    else
+        cat "${BASE_DIR}/${1}"
+    fi    
+}
+
+
+export REPO_RAW_URL
+# banner_content=$(get_file "/assets/banner.txt" | envsubst)   
+# echo "$banner_content"
+get_file "/assets/banner.txt" | envsubst
+
 source "$SCRIPT_DIR/src/load.sh"
 
+exit;
+cmd="${cmd:-$1}"
 
-
-opt="$1"
+opt="$cmd" #"$1"
 choice=$( tr '[:upper:]' '[:lower:]' <<<"$opt" )
 case $choice in
     multipass) _multipass "$@" ;;
--- src/load.sh
+++ src/load.sh
@@ -3,5 +3,6 @@
 ## To get all functions : bash -c "source src/load.bash && declare -F"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+
 # shellcheck source=/dev/null
 source "$SCRIPT_DIR/common/host.sh"
